581501780790bccf20fdf256ecac3b997e32789f
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-07-29 20:26:47 +0530

    Added Challenge 17

---
+++ 17-Sort-Without-Articles/js/main.js
@@ -0,0 +1,13 @@
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const strip = (bandName) => {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector('.list').innerHTML = sortedBands.map(band =>
+    `<li class="list-item">
+        <p>${band}</p>
+    </li>`)
+    .join('');F
