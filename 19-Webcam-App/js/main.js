16248ca137ebde8a3b6e38614d654a4de75d872a
Satvik Chachra <50296407+satvikchachra@users.noreply.github.com>
2020-08-11 23:38:48 +0530

    Added Filters Functionality

--- 19-Webcam-App/js/main.js
+++ 19-Webcam-App/js/main.js
@@ -3,6 +3,31 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+let effect = null;
+
+document.querySelector('#rBtn').addEventListener('click', () => {
+    effect = "redEffect";
+    console.log(effect);
+    paintToCanvas();
+});
+
+document.querySelector('#gBtn').addEventListener('click', () => {
+    effect = "rgbSplit";
+    console.log(effect);
+    paintToCanvas();
+});
+
+document.querySelector('#gsBtn').addEventListener('click', () => {
+    effect = "greenScreen";
+    console.log(effect);
+    paintToCanvas();
+});
+
+document.querySelector('#nBtn').addEventListener('click', () => {
+    effect = "null";
+    console.log(effect);
+    paintToCanvas();
+});
 
 const getVideo = () => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -12,7 +37,7 @@ const getVideo = () => {
             video.play();
         })
         .catch(err => console.log(err));
-}
+};
 
 const paintToCanvas = () => {
     const width = video.videoWidth;
@@ -22,21 +47,100 @@ const paintToCanvas = () => {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+
+        // Capture the pixels
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // Perform various operations on the pixels
+
+        switch (effect) {
+            case 'redEffect': {
+                pixels = redEffect(pixels);
+                break;
+            }
+            case 'rgbSplit': {
+                pixels = rgbSplit(pixels);
+                break;
+            }
+            case 'greenScreen': {
+                pixels = greenScreen(pixels);
+                break;
+            }
+        }
+
+        // Red Effect
+        // pixels = redEffect(pixels);
+
+        // Ghosting Effect
+        // pixels = rgbSplit(pixels);
+        // ctx.globalAlpha = 0.8;
+
+        // Green Screen
+        // pixels = greenScreen(pixels);
+
+        // Put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
 const takePhoto = () => {
+
+    // To play sound
     snap.currentTime = 0;
     snap.play();
 
+    // Take the data out of the canvas
     const data = canvas.toDataURL('image/jpeg');
-    console.log(data);
-
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'handsome');
-    link.textContent = 'Download image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+const redEffect = (pixels) => {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+        pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
+const rgbSplit = (pixels) => {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // RED
+        pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    }
+    return pixels;
+}
+
+const greenScreen = (pixels) => {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
 }
 
 getVideo();
